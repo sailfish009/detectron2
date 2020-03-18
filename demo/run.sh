9e5386a680588b06c08be030aab219970b749427
sailfish009 <sailfish009@gmail.com>
2020-03-18 17:42:54 +0900

    update demo

---
+++ demo/run.sh
@@ -0,0 +1,3 @@
+python demo.py --config-file ../configs/COCO-InstanceSegmentation/mask_rcnn_R_50_FPN_3x.yaml \
+  --input input1.jpg input2.jpg \
+  --opts MODEL.WEIGHTS detectron2://COCO-InstanceSegmentation/mask_rcnn_R_50_FPN_3x/137849600/model_final_f10217.pkl
